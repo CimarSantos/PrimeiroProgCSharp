7855960d2a3d4607c5632c2e049e483cff3116bf
Francimar Santos <francimarlsantos@outlook.com>
2021-01-28 15:51:35 -0300

    Programa Finalizado

--- Program.cs
+++ Program.cs
@@ -6,6 +6,79 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do Aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Qual a nota do aluno?:");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota)){
+                            aluno.Nota = nota;
+                        } else {
+                            throw new ArgumentException("O valor da nota deve ser em forma decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
+                        break;
+                    case "2":
+                        foreach (var a in alunos){
+                            if (!string.IsNullOrEmpty(a.Nome)){
+                            Console.WriteLine($"Aluno: {a.Nome} - Nota: {a.Nota}");
+                            }
+                        }
+                        break;
+                    case "3":
+                    decimal notaTotal =0;
+                    var nrAlunos =0;
+
+                        for(int i=0; i < alunos.Length; i++){
+                            if (!string.IsNullOrEmpty(alunos[i].Nome)){
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+
+                            var mediaGeral = notaTotal / nrAlunos;
+                            Conceito conceitoGeral;
+
+                            if (mediaGeral < 2){
+                                conceitoGeral = Conceito.E;
+                            } else if (mediaGeral < 4){
+                                conceitoGeral = Conceito.D;
+                            } else if (mediaGeral < 6){
+                                conceitoGeral = Conceito.C;
+                            } else if (mediaGeral < 8){
+                                conceitoGeral = Conceito.B;
+                            } else {
+                                conceitoGeral = Conceito.A;
+                            }
+
+                            Console.WriteLine($"A Média geral é: {mediaGeral} - Conceito Geral: {conceitoGeral}");
+
+                        break;
+                    default:
+
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
             Console.WriteLine("Informe a Opção desejada:");
             Console.WriteLine("1 - Inserir novo aluno");
             Console.WriteLine("2 - Listar Alunos");
@@ -14,23 +87,7 @@ namespace Revisao
             Console.WriteLine();
 
             string opcaoUsuario = Console.ReadLine();
-
-            while (opcaoUsuario.ToUpper() != "X"){
-                switch(opcaoUsuario){
-                    case "1":
-
-                        break;
-                    case "2":
-
-                        break;
-                    case "3":
-
-                        break;
-                    default:
-
-                    throw new ArgumentOutOfRangeException();
-                }
-            }
+            return opcaoUsuario;
         }
     }
 }
